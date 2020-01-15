82d11a2142ff31f840872ea81d9b6c7eb0ee6b50
DavidKroon <david@kroonstudio.com>
2020-01-15 09:07:51 +0100

    First commit

--- Articles/admin.py
+++ Articles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from Articles.models import Article
+from Articles.models import Article,ArticleTest
 # Register your models here.
 
 class ArticleAdmnin(admin.ModelAdmin):
     pass
 admin.site.register(Article,ArticleAdmnin)
+admin.site.register(ArticleTest)
